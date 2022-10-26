578be847d5beeeced3c5459ce88d7fc69191f870
Vaibhav Gavali <vaibhavgavali92@gmail.com>
2022-10-26 22:30:48 +0530

    Added social links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a
+          href={'https://www.instagram.com/masum_vaibhav/'}
+          target="_black"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
       </div>
       <div>
-        <BsInstagram />
-      </div>
-      <div>
-        <FaFacebookF />
+        <a
+          href={'https://www.facebook.com/vaibhav.gavali.31'}
+          target="_black"
+          rel="noreferrer"
+        >
+          <FaFacebookF />
+        </a>
       </div>
     </div>
   );
